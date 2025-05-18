8e4dcb12b4050ed71f89086e3b1959f51354ac44
jessicamehra <jessicamehra01@gmail.com>
2025-05-18 10:26:03 +0530

    final

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log ("hello world")\
+
+var stu_name= "Jessica";
+console.log ("my name is", stu_name);
+
+let stu_id= "12344578";
+console.log (stu_id);
+
+const stu_contact= "4657832649";
+console.log (stu_contact);
